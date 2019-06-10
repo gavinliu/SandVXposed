f7e3260289404255efcef0b387e60a544cd86b9e
Gavin Liu <gavin6liu@gmail.com>
2019-06-10 17:12:31 +0800

    feat: update virtual daemon service notification style

--- lib/src/main/java/com/lody/virtual/client/stub/DaemonService.java
+++ lib/src/main/java/com/lody/virtual/client/stub/DaemonService.java
@@ -6,44 +6,50 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 
+import com.lody.virtual.R;
+
 
 /**
  * @author Lody
- *
  */
 public class DaemonService extends Service {
 
     private static final int NOTIFY_ID = 1001;
 
-	public static void startup(Context context) {
-		context.startService(new Intent(context, DaemonService.class));
-	}
+    public static void startup(Context context) {
+        context.startService(new Intent(context, DaemonService.class));
+    }
 
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		startup(this);
-	}
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        startup(this);
+    }
 
-	@Override
-	public IBinder onBind(Intent intent) {
-		return null;
-	}
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 
-	@Override
-	public void onCreate() {
-		super.onCreate();
-        startService(new Intent(this, InnerService.class));
-        startForeground(NOTIFY_ID, new Notification());
+    @Override
+    public void onCreate() {
+        super.onCreate();
+//        startService(new Intent(this, InnerService.class));
 
-	}
+        Notification notification = new Notification.Builder(this)
+                .setContentTitle("虚拟化容器运行中...")
+                .setSmallIcon(R.drawable.ic_ac_unit_black_24dp)
+                .build();
 
-	@Override
-	public int onStartCommand(Intent intent, int flags, int startId) {
-		return START_STICKY;
-	}
+        startForeground(NOTIFY_ID, notification);
+    }
 
-	public static final class InnerService extends Service {
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+
+    public static final class InnerService extends Service {
 
         @Override
         public int onStartCommand(Intent intent, int flags, int startId) {
@@ -53,11 +59,11 @@ public class DaemonService extends Service {
             return super.onStartCommand(intent, flags, startId);
         }
 
-		@Override
-		public IBinder onBind(Intent intent) {
-			return null;
-		}
-	}
+        @Override
+        public IBinder onBind(Intent intent) {
+            return null;
+        }
+    }
 
 
 }
